688cec58f48735df8d2d93b0ef4a7717fcb3aeb9
BergetAlex <alex.bergkrantz@gmail.com>
2024-02-14 13:24:17 +0100

    fixed code

--- main.c
+++ main.c
@@ -4,8 +4,8 @@ int main() {
     int a = 2;
     int b = 3;
     int sum;
-    sum = a+b;
+    sum = a + b;
     printf("Hello, World\n");
-    printf("%d");
+    printf("%d", sum);
     return 0;
 }
